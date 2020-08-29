f4be90c24baa3794f2979cb58fff7589e1b38c76
Khang Do <khangdohuu.vccb@gmail.com>
2020-08-29 19:29:34 -0400

    187. push(), pop(), unshift(), shift() - Adding & Removing Elements

--- app.js
+++ app.js
@@ -1,38 +1,53 @@
 // Creating arrays
-const numbers = [1, 2, 3]; // Better performance perspective
-console.log(numbers);
+// const numbers = [1, 2, 3]; // Better performance perspective
+// console.log(numbers);
 
-// const moreNumbers = new Array(5); // == []
-// const someNumbers = Array(5, 2);
+// // const moreNumbers = new Array(5); // == []
+// // const someNumbers = Array(5, 2);
 
-// console.log(moreNumbers);
-// console.log(someNumbers);
+// // console.log(moreNumbers);
+// // console.log(someNumbers);
 
-// const yetMoreNumbers = Array.of(1, 2);
-// console.log(yetMoreNumbers);
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers);
 
-const listItems = document.querySelectorAll('li');
-console.log(listItems);
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
 
-const moreNumbers = Array.from('Hi!');
-const someItems = Array.from(listItems);
-console.log(moreNumbers, someItems);
+// const moreNumbers = Array.from('Hi!');
+// const someItems = Array.from(listItems);
+// console.log(moreNumbers, someItems);
 
-// Data Can You Store In Arrays
-const hobbies = ['Cooking', 'Sports'];
-const personalData = [30, 'Max', { moreDetail: [] }];
+// // Data Can You Store In Arrays
+// const hobbies = ['Cooking', 'Sports'];
+// const personalData = [30, 'Max', { moreDetail: [] }];
 
-// Nested arrays
-const analyticsData = [
-    [1, 1.6],
-    [-5.4, 2.1]
-];
+// // Nested arrays
+// const analyticsData = [
+//     [1, 1.6],
+//     [-5.4, 2.1]
+// ];
 
-// Accessing data in a nested array
-for (const data of analyticsData) {
-    for (const dataPoint of data) {
-        console.log(dataPoint);
-    };
-};
+// // Accessing data in a nested array
+// for (const data of analyticsData) {
+//     for (const dataPoint of data) {
+//         console.log(dataPoint);
+//     };
+// };
 
-console.log(personalData[1]);
+// console.log(personalData[1]);
+
+// Adding elements
+const hobbies = ['Sports', 'Cooking'];
+hobbies.push('Reading');
+hobbies.unshift('Coding');
+
+
+// Removing elements
+const poppedValue = hobbies.pop();
+hobbies.shift();
+console.log(hobbies);
+
+hobbies[1] = 'Coding';
+// hobbies[5] = 'Reading'; // Rarely used
+console.log(hobbies, hobbies[4]);
