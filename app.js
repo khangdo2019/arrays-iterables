b1ac75d444116557ebdb6f563704b53eeaea61b9
Khang Do <khangdohuu.vccb@gmail.com>
2020-09-15 17:21:07 -0400

    203. Understanding Array Destructuring

--- app.js
+++ app.js
@@ -119,59 +119,107 @@
 // console.log(taxAdjustedPrices);
 
 // 195. Transforming Data with map()
-const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
 
-const taxAdjustedPrices = prices.map((price, idx, prices) => {
-    const priceObj = { index: idx, taxAdjustedPrices: price * (1 + tax) };
-    return priceObj;
-});
-
-console.log(prices, taxAdjustedPrices);
-
-// 196. sort()ing & reverse()ing
-// const sortedPrices = prices.sort(); ==> (4) [10.99, 3.99, 5.99, 6.59]
-const sortedPrices = prices.sort((a, b) => {
-    if (a > b) {
-        return 1;
-    } else if (a === b) {
-        return 0;
-    } else {
-        return -1;
-    }
-});
-
-console.log(sortedPrices);
-
-console.log(sortedPrices.reverse()); // An alternative to reverse a sorted list
-
-// 197. Filtering Arrays & filter()
-const filteredArray = prices.filter((price, index, prices) => {
-    return price > 6; // ==> (2) [10.99, 6.59]
-});
-
-console.log(filteredArray);
-
-// 198. Where Arrow Functions Shine!
-const filteredArrayShorten = prices.filter(p => p > 6); // ==> super short & concise
-
-console.log(filteredArrayShorten);
-
-// 199. The Important reduce() Method
-// Using forEach method to calculate a sum of an array
-// let sum = 0;
-
-// prices.forEach((price) => {
-//     sum += price;
+// const taxAdjustedPrices = prices.map((price, idx, prices) => {
+//     const priceObj = { index: idx, taxAdjustedPrices: price * (1 + tax) };
+//     return priceObj;
 // });
+
+// console.log(prices, taxAdjustedPrices);
+
+// // 196. sort()ing & reverse()ing
+// // const sortedPrices = prices.sort(); ==> (4) [10.99, 3.99, 5.99, 6.59]
+// const sortedPrices = prices.sort((a, b) => {
+//     if (a > b) {
+//         return 1;
+//     } else if (a === b) {
+//         return 0;
+//     } else {
+//         return -1;
+//     }
+// });
+
+// console.log(sortedPrices);
+
+// console.log(sortedPrices.reverse()); // An alternative to reverse a sorted list
+
+// // 197. Filtering Arrays & filter()
+// const filteredArray = prices.filter((price, index, prices) => {
+//     return price > 6; // ==> (2) [10.99, 6.59]
+// });
+
+// console.log(filteredArray);
+
+// // 198. Where Arrow Functions Shine!
+// const filteredArrayShorten = prices.filter(p => p > 6); // ==> super short & concise
+
+// console.log(filteredArrayShorten);
+
+// // 199. The Important reduce() Method
+// // Using forEach method to calculate a sum of an array
+// // let sum = 0;
+
+// // prices.forEach((price) => {
+// //     sum += price;
+// // });
+
+// // console.log(sum);
+
+// // const sum = prices.reduce((preValue, curValue, curIndex, prices) => {
+// //     return preValue + curValue;
+// // }, 0);
+
+// // Shorten form
+// const sum = prices.reduce((preValue, curValue) => preValue + curValue, 0);
 
 // console.log(sum);
 
-// const sum = prices.reduce((preValue, curValue, curIndex, prices) => {
-//     return preValue + curValue;
-// }, 0);
+// // 201. Arrays & Strings - split() & join()
+// const data = 'New York;10.99;2000';
 
-// Shorten form
-const sum = prices.reduce((preValue, curValue) => preValue + curValue, 0);
+// // Using split to split an array
+// const transformedData = data.split(';');
 
-console.log(sum);
+// // Try to convert elements in the transformed data
+// // transformedData[1] = +transformedData[1];
+// const mapData = transformedData.map((el) => {
+//     if (!isNaN(+el)) {
+//         return +el;
+//     } else { return el; }
+// });
+
+// console.log(transformedData, mapData);
+
+// // Using join method
+// const nameFragments = ['Max', 'Schwarz'];
+// const name = nameFragments.join(' ');
+
+// console.log(name);
+
+// // 202. The Spread Operator (...)
+// const copiedNameFragments = [...nameFragments];
+// nameFragments.push('Mr');
+// console.log(nameFragments, copiedNameFragments);
+
+// console.log(Math.min(...prices));
+
+// const persons = [{ name: 'Max', age: 30 }, { name: 'Manuel', age: 31 }];
+// // const copiedPersons = [...persons];
+// const copiedPersons = [...persons.map(person => ({ name: person.name, age: person.age }))];
+// persons[0].age = 31;
+
+// persons.push({ name: 'Anna', age: 29 });
+
+// console.log(persons, copiedPersons);
+
+// 203. Understanding Array Destructuring
+const nameData = ['Max', 'Schwarz', 'Mr', 30];
+// Long way to get the name elements
+// const firstName = nameData[0];
+// const lastName = nameData[1];
+
+// Shortcut for the above lines of code
+const [firstName, lastName, ...otherInformation] = nameData;
+console.log(firstName, lastName, otherInformation);
